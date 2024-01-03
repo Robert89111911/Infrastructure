1c421b79d5ccbe7fc0e64dc3e5307853a80aa72d
Robert <tarnawskirobert@gmail.com>
2024-01-03 17:22:52 +0000

    have added parameters

--- jenkins_controller/jobs/app_of_apps.groovy
+++ jenkins_controller/jobs/app_of_apps.groovy
@@ -1,4 +1,8 @@
 pipelineJob('app_of_apps') {
+    parameters {
+        stringParam('backendDockerTag', '', 'Backend docker image tag')
+        stringParam('frontendDockerTag', '', 'Frontend docker image tag')
+    }
     definition {
         cpsScm {
             scm {
